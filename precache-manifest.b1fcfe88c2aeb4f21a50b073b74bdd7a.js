6f0f46b8a08da90213839dbc1612475b8bb4b362
Anikatya <lopukhina.ekaterina@gmail.com>
2020-07-07 17:19:36 +0300

    Updates

---
+++ precache-manifest.b1fcfe88c2aeb4f21a50b073b74bdd7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67e0e103041b9ee3c8785604ed9107d",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "9a6cfebdfcf177f7d88c",
+    "url": "/react-calculator/static/css/main.bb56e56b.chunk.css"
+  },
+  {
+    "revision": "aae5c370158c8b125e83",
+    "url": "/react-calculator/static/js/2.ab10b74f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-calculator/static/js/2.ab10b74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6cfebdfcf177f7d88c",
+    "url": "/react-calculator/static/js/main.a88408ab.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  },
+  {
+    "revision": "4d057181f4654bec8dc9ae0e236a3389",
+    "url": "/react-calculator/static/media/Orbitron-Medium.4d057181.ttf"
+  }
+]);
